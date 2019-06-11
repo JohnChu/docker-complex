73f861437dc118be4a586c0ab5b970d2821c53ff
John Chu <johnchu@hotmail.com>
2019-06-10 21:50:38 -0700

    dockerizing multiple services

--- worker/index.js
+++ worker/index.js
@@ -2,23 +2,18 @@ const keys = require('./keys');
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
 });
 const sub = redisClient.duplicate();
 
 function fib(index) {
-    if (index < 2) return 1;
-    return fib(index -1) + fib(index -2);
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
-/*
-Subscribe to the message event, then feed it through the
-callback function.
-*/
 sub.on('message', (channel, message) => {
-    // Save the hash of message and fib result to Redis
-    redisClient.hset('values', message, fib(parseInt(message)));
+  redisClient.hset('values', message, fib(parseInt(message)));
 });
 sub.subscribe('insert');
